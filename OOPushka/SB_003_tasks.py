e60ae82b9c42107d98c5ef27bd2c899228078d28
Grigory Kozhanov <kogriv@gmail.com>
2023-09-29 15:31:29 +0700

    oopushke SB course

--- OOPushka/SB_003_tasks.py
+++ OOPushka/SB_003_tasks.py
@@ -197,28 +197,170 @@
 # cart.add(Cup('coboyashi',10))
 
 # 9
-# Односвязный список
-import sys
-class ListObject:
-    def __init__(self, data):
-        self.data = data
-        self.next_obj = None
+# # Односвязный список
+# import sys
+# class ListObject:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next_obj = None
+#
+#     # для присоединения объекта obj такого
+#     # же класса к текущему объекту self
+#     def link(self, obj):
+#         self.next_obj = obj
+#
+# # считывание списка из входного потока
+# # (эту строкуне менять)
+# lst_in = list(map(str.strip, sys.stdin.readlines())) # список lst_in в программе не менять
+#
+# # На первый добавленный объект класса ListObject
+# # должна ссылаться переменная head_obj
+# # первая строка в первом объекте
+# head_obj = ListObject(lst_in[0])
+# obj = head_obj
+# for i in range(1, len(lst_in)):
+#     obj_new = ListObject(lst_in[i])
+#     obj.link(obj_new)
+#     obj = obj_new
+# ****************************************************
+# ****************************************************
+# ****************************************************
+#
+# 10
+# Объявите два класса:
+#
+# Cell - для представления клетки игрового поля;
+# GamePole - для управления игровым полем, размером
+# N x N клеток.
+#
+# С помощью класса Cell предполагается создавать
+# отдельные клетки командой:
+#
+# c1 = Cell(around_mines, mine)
+#
+# Здесь around_mines - число мин вокруг данной клетки поля;
+# mine - булева величина (True/False), означающая
+# наличие мины в текущей клетке. При этом, в
+# каждом объекте класса Cell должны создаваться локальные
+# свойства:
+#
+# around_mines - число мин вокруг клетки
+#                (начальное значение 0);
+# mine - наличие/отсутствие мины в текущей клетке
+#        (True/False);
+# fl_open - открыта/закрыта клетка - булево значение
+#           (True/False).
+#           Изначально все клетки закрыты (False).
+#
+# С помощью класса GamePole должна быть возможность
+# создавать квадратное игровое поле с числом клеток N x N:
+#
+# pole_game = GamePole(N, M)
+#
+# Здесь N - размер поля; M - общее число мин на поле.
+# При этом, каждая клетка представляется объектом
+# класса Cell и все объекты хранятся в двумерном
+# списке N x N элементов - локальном свойстве pole
+# объекта класса GamePole.
+#
+# В классе GamePole должны быть также реализованы
+# следующие методы:
+#
+# init() - инициализация поля с новой расстановкой
+#          M мин (случайным образом по игровому полю,
+#          разумеется каждая мина должна находиться в
+#          отдельной клетке).
+# show() - отображение поля в консоли в виде таблицы
+#          чисел открытых клеток (если клетка не открыта,
+#          то отображается символ #;
+#          мина отображается символом *;
+#          между клетками при отображении ставить пробел).
+#
+# При создании экземпляра класса GamePole в его
+# инициализаторе следует вызывать метод init() для
+# первоначальной инициализации игрового поля.
+#
+# В классе GamePole могут быть и другие вспомогательные
+# методы.
+#
+# Создайте экземпляр pole_game класса GamePole
+# с размером поля N = 10 и числом мин M = 12.
 
-    # для присоединения объекта obj такого
-    # же класса к текущему объекту self
-    def link(self, obj):
-        self.next_obj = obj
+from random import randint
+class Cell:
+    def __init__(self, around_mines: int = 0,
+                 mine: bool = False
+                 ):
+        self.around_mines = around_mines
+        self.mine = mine
+        self.fl_open = True
 
-# считывание списка из входного потока
-# (эту строкуне менять)
-lst_in = list(map(str.strip, sys.stdin.readlines())) # список lst_in в программе не менять
 
-# На первый добавленный объект класса ListObject
-# должна ссылаться переменная head_obj
-# первая строка в первом объекте
-head_obj = ListObject(lst_in[0])
-obj = head_obj
-for i in range(1, len(lst_in)):
-    obj_new = ListObject(lst_in[i])
-    obj.link(obj_new)
-    obj = obj_new
+class GamePole:
+    def __init__(self, N, M):
+        self._n = N
+        self._m = M
+        self.pole = [
+            [Cell() for column in range(self._n)]
+            for row in range(self._n)
+        ]
+        self.init()
+        # print(pole_matrix)
+
+    # расстановка мин
+    def init(self):
+        m = 0
+        while m < self._m:
+            column = randint(0, self._n - 1)
+            row = randint(0, self._n - 1)
+            if self.pole[row][column].mine:
+                continue
+            self.pole[row][column].mine = True
+            m += 1
+        # сдвиги индексов вокруг выбранной клетки
+        # аффинное преобразование
+        idx_shift = ((-1,-1), (-1,0), (-1,1),
+                     (0 ,-1),         (0 ,1),
+                     (1 ,-1), (1 ,0), (1 ,1))
+        # проверка мин вокруг клеток
+        for row in range(self._n):
+            for column in range(self._n):
+                # считаем мины только вокруг
+                # не заминированных клеток (?)
+                if not self.pole[row][column].mine:
+                    mines_around_selected_cell = sum(
+                (self.pole[row+row_shift][column+column_shift].mine
+                 for row_shift, column_shift in idx_shift
+                 if 0 <= row+row_shift < self._n and
+                    0 <= column+column_shift <self._n)
+                )
+                    self.pole[row][column].around_mines = \
+                        mines_around_selected_cell
+                    # print('cell: ',row+1,column+1, 'mac:',
+                    #       self.pole[row][column].around_mines)
+
+    def show(self):
+        for row in self.pole:
+            print(*map(lambda x:
+                    '#' if not x.fl_open
+                    else x.around_mines
+                        # if not x.mine else '*'
+                       , row))
+            # print(*map(lambda x: x.around_mines
+            #             , row))
+        print('================================')
+        for row in self.pole:
+            print(*map(lambda x:
+                    '#' if not x.fl_open
+                    else x.around_mines
+                        if not x.mine else '*'
+                       , row))
+            # print(*map(lambda x: x.around_mines
+            #             , row))
+        print('========self.pole[0][0]=========')
+        print(self.pole[0][0])
+        print(id(self.pole[0][0]))
+        print(hex(id(self.pole[0][0])))
+
+pole_game = GamePole(2, 1)
+pole_game.show()
