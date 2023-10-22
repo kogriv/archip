0feabafef2c6291ca4fc470ec4525b008f3d01b0
Grigory Kozhanov <kogriv@gmail.com>
2023-10-22 18:51:47 +0700

    oop course

---
+++ OOPushka/SB_021_issubclass.py
@@ -0,0 +1,497 @@
+# 3
+# Создается проект, в котором предполагается
+# использовать списки из целых чисел. Для
+# этого вам ставится задача создать класс
+# с именем ListInteger с базовым классом
+# list и переопределить три метода:
+#
+# __init__()
+# __setitem__()
+# append()
+#
+# так, чтобы список ListInteger содержал
+# только целые числа. При попытке присвоить
+# любой другой тип данных, генерировать
+# исключение командой:
+#
+# raise TypeError('можно передавать только целочисленные значения')
+#
+# Пример использования класса ListInteger
+# (эти строчки в программе не писать):
+#
+# s = ListInteger((1, 2, 3))
+# s[1] = 10
+# s.append(11)
+# print(s)
+# s[0] = 10.5 # TypeError
+#
+# P.S. В программе нужно объявить
+# только класс. На экран выводить
+# ничего не нужно.
+# class ListInteger(list):
+#     def __init__(self, initial_list=()):
+#         for item in initial_list:
+#             self.check_integer(item)
+#             super().__init__(initial_list)
+#
+#     def __setitem__(self, key, value):
+#         self.check_integer(value)
+#         super().__setitem__(key, value)
+#
+#     def append(self, value):
+#         self.check_integer(value)
+#         super().append(value)
+#
+#     def check_integer(self, value):
+#         if not isinstance(value, int):
+#             raise TypeError('можно передавать только целочисленные значения')
+
+# 4
+# Разрабатывается интернет-магазин. Каждый
+# товар предполагается представлять классом
+# Thing, объекты которого создаются командой:
+#
+# thing = Thing(name, price, weight)
+#
+# где
+# name - наименование товара (строка);
+# price - цена (вещественное число);
+# weight - вес товара (вещественное число).
+# В каждом объекте этого класса создаются
+# аналогичные атрибуты: name, price, weight.
+#
+# Класс Thing необходимо определить так,
+# чтобы его объекты можно было использовать
+# в качестве ключей словаря, например:
+#
+# d = {}
+# d[thing] = thing
+#
+# И для каждого уникального набора данных
+# name, price, weight должны формироваться
+# свои уникальные ключи.
+#
+# Затем, вам необходимо объявить класс
+# словаря DictShop, унаследованный от
+# базового класса dict. В этом новом
+# словаре ключами могут выступать только
+# объекты класса Thing. При попытке
+# указать любой другой тип, генерировать
+# исключение командой:
+#
+# raise TypeError('ключами могут быть только объекты класса Thing')
+#
+# Объекты класса DictShop должны
+# создаваться командами:
+#
+# dict_things = DictShop() # пустой словарь
+# dict_things = DictShop(things) # словарь с набором словаря things
+#
+# где things - некоторый словарь.
+# В инициализаторе следует проверять,
+# чтобы аргумент thing был словарем,
+# если не так, то выбрасывать исключение:
+#
+# raise TypeError('аргумент должен быть словарем')
+#
+# И проверять, чтобы все ключи являлись
+# объектами класса Thing. Если это не
+# так, то генерировать исключение:
+#
+# raise TypeError('ключами могут быть только объекты класса Thing')
+#
+# Дополнительно в классе DictShop
+# переопределить метод:
+#
+# __setitem__()
+#
+# с проверкой, что создаваемый ключ является
+# объектом класса Thing. Иначе, генерировать
+# исключение:
+#
+# raise TypeError('ключами могут быть толькообъекты класса Thing')
+#
+# Пример использования классов
+# (эти строчки в программе не писать):
+#
+# th_1 = Thing('Лыжи', 11000, 1978.55)
+# th_2 = Thing('Книга', 1500, 256)
+# dict_things = DictShop()
+# dict_things[th_1] = th_1
+# dict_things[th_2] = th_2
+#
+# for x in dict_things:
+#     print(x.name)
+#
+# dict_things[1] = th_1 # исключение TypeError
+#
+# P.S. В программе нужно объявить
+# только классы. На экран выводить
+# ничего не нужно.
+
+# class Thing:
+#     def __init__(self, name, price, weight):
+#         self.name = name
+#         self.price = price
+#         self.weight = weight
+#
+#     def __hash__(self):
+#         return hash((self.name, self.price, self.weight))
+#
+#     def __eq__(self, other):
+#         return (self.name, self.price, self.weight) == (other.name, other.price, other.weight)
+#
+#
+# class DictShop(dict):
+#     def __init__(self, things=None):
+#         if things is not None and not isinstance(things, dict):
+#             raise TypeError('аргумент должен быть словарем')
+#         if things is not None:
+#             for key in things.keys():
+#                 if not isinstance(key, Thing):
+#                     raise TypeError('ключами могут быть только объекты класса Thing')
+#             super().__init__(things)
+#
+#     def __setitem__(self, key, value):
+#         if not isinstance(key, Thing):
+#             raise TypeError('ключами могут быть только объекты класса Thing')
+#         super().__setitem__(key, value)
+
+# Объявите в программе следующие классы без
+# содержимого (используйте оператор pass):
+#
+# Protists, Plants, Animals, Mosses, Flowering,
+# Worms, Mammals, Human, Monkeys
+#
+# и постройте схему наследования в соответствии
+# со следующей иерархией древа жизни:
+# Human -> Mammals
+# Monkeys -> Mammals
+# Mammals -> Animals
+# Worms -> Animals
+# Flowering -> Plants
+# Mosses -> Plants
+# Animals -> Protists
+# Plants -> Protists
+# Затем, объявите в программе классы:
+#
+# Monkey - наследуется от Monkeys и служит для описания обезьян;
+# Person - наследуется от Human и служит для описания человека;
+# Flower - наследуется от Flowering и служит для описания цветка;
+# Worm - наследуется от Worms и служит для описания червей.
+#
+# Объекты этих классов должны создаваться командами:
+#
+# obj = Monkey(name, weight, old)
+# obj = Person(name, weight, old)
+# obj = Flower(name, weight, old)
+# obj = Worm(name, weight, old)
+#
+# где
+# name - наименование (или имя) объекта (строка);
+# weight - вес (вещественное число);
+# old - возраст (целое число). В каждом объекте
+# любого из этих классов должны создаваться
+# соответствующие атрибуты: name, weight, old.
+#
+# Создайте в программе следующие объекты и
+# сохраните их в виде списка lst_objs:
+#
+# Monkey: "мартышка", 30.4, 7
+# Monkey: "шимпанзе", 24.6, 8
+# Person: "Балакирев", 88, 34
+# Person: "Верховный жрец", 67.5, 45
+# Flower: "Тюльпан", 0.2, 1
+# Flower: "Роза", 0.1, 2
+# Worm: "червь", 0.01, 1
+# Worm: "червь 2", 0.02, 1
+#
+# Затем, используя функции isinstance()
+# и генератор списков (List comprehensions),
+# сформируйте следующие списки из указанных объектов:
+#
+# lst_animals - все объекты, относящиеся к животным (Animals);
+# lst_plants - все объекты, относящиеся к растениям (Plants);
+# lst_mammals - все объекты, относящиеся к млекопитающим (Mammals).
+#
+# P.S. В программе на экран выводить ничего не нужно.
+
+# class Protists:
+#     pass
+#
+# class Plants(Protists):
+#     pass
+#
+# class Animals(Protists):
+#     pass
+#
+# class Mosses(Plants):
+#     pass
+#
+# class Flowering(Plants):
+#     pass
+#
+# class Worms(Animals):
+#     pass
+#
+# class Mammals(Animals):
+#     pass
+#
+# class Human(Mammals):
+#     pass
+#
+# class Monkeys(Mammals):
+#     pass
+#
+# class Monkey(Monkeys):
+#     def __init__(self, name, weight, old):
+#         self.name = name
+#         self.weight = weight
+#         self.old = old
+#
+# class Person(Human):
+#     def __init__(self, name, weight, old):
+#         self.name = name
+#         self.weight = weight
+#         self.old = old
+#
+# class Flower(Flowering):
+#     def __init__(self, name, weight, old):
+#         self.name = name
+#         self.weight = weight
+#         self.old = old
+#
+# class Worm(Worms):
+#     def __init__(self, name, weight, old):
+#         self.name = name
+#         self.weight = weight
+#         self.old = old
+#
+#
+# # Создание объектов
+# lst_objs = [
+#     Monkey("мартышка", 30.4, 7),
+#     Monkey("шимпанзе", 24.6, 8),
+#     Person("Балакирев", 88, 34),
+#     Person("Верховный жрец", 67.5, 45),
+#     Flower("Тюльпан", 0.2, 1),
+#     Flower("Роза", 0.1, 2),
+#     Worm("червь", 0.01, 1),
+#     Worm("червь 2", 0.02, 1)
+# ]
+#
+# # Генерация списков
+# lst_animals = [obj for obj in lst_objs if isinstance(obj, Animals)]
+# lst_plants = [obj for obj in lst_objs if isinstance(obj, Plants)]
+# lst_mammals = [obj for obj in lst_objs if isinstance(obj, Mammals)]
+
+# 6
+# Известно, что с объектами класса tuple можно
+# складывать только такие же объекты (кортежи).
+# Например:
+#
+# t1 = (1, 2, 3)
+# t2 = t1 + (4, 5) # (1, 2, 3, 4, 5)
+#
+# Если же мы попытаемся прибавить любой другой
+# итерируемый объект, например, список:
+#
+# t2 = t1 + [4, 5]
+#
+# то возникнет ошибка. Предлагается поправить
+# этот функционал и создать свой собственный
+# класс Tuple, унаследованный от базового класса
+# tuple и поддерживающий оператор:
+#
+# t1 = Tuple(iter_obj)
+# t2 = t1 + iter_obj  # создается новый объект класса Tuple с новым (соединенным) набором данных
+#
+# где iter_obj - любой итерируемый объект
+# (список, словарь, строка, множество, кортеж и т.п.)
+#
+# Пример использования класса
+# (эти строчки в программе не писать):
+#
+# t = Tuple([1, 2, 3])
+# t = t + "Python"
+# print(t)   # (1, 2, 3, 'P', 'y', 't', 'h', 'o', 'n')
+# t = (t + "Python") + "ООП"
+#
+# P.S. В программе нужно объявить
+# только класс. На экран выводить
+# ничего не нужно.
+
+# class Tuple(tuple):
+#     def __new__(cls, iter_obj):
+#         if not isinstance(iter_obj, (list, tuple, str, set, dict)):
+#             raise TypeError("Можно использовать только итерируемые объекты")
+#         return super(Tuple, cls).__new__(cls, iter_obj)
+#
+#     def __add__(self, other):
+#         if not isinstance(other, (list, tuple, str, set, dict)):
+#             raise TypeError("Можно складывать только с другими итерируемыми объектами")
+#         return Tuple(list(self) + list(other))
+
+# 7
+# Необходимо в программе объявить класс VideoItem
+# для представления одного видео (например, в youtube).
+# Объекты этого класса должны создаваться командой:
+#
+# video = VideoItem(title, descr, path)
+#
+# где
+# title - заголовок видео (строка);
+# descr - описание видео (строка);
+# path - путь к видеофайлу.
+# В каждом объекте класса VideoItem должны
+# создаваться соответствующие атрибуты:
+# title, descr, path.
+#
+# Затем, нужно создать класс для формирования
+# оценки видео в баллах от 0 до 5. Для этого
+# нужно объявить еще один класс с именем
+# VideoRating, объекты которого создаются
+# командой:
+#
+# rating = VideoRating()
+#
+# В каждом объекте класса VideoRating должен
+# быть локальный приватный атрибут с именем
+# __rating, содержащий целое число от 0 до 5
+# (по умолчанию 0). А для записи и считывания
+# значения из этого приватного атрибута должно
+# быть объект-свойство (property) с именем rating.
+#
+# Так как атрибут __rating - это целое число
+# в диапазоне [0; 5], то в момент присвоения
+# ему какого-либо значения необходимо проверять,
+# что присваиваемое значение - целое число в
+# диапазоне [0; 5]. Если это не так, то
+# генерировать исключение командой:
+#
+# raise ValueError('неверное присваиваемое значение')
+#
+# Далее, в каждом объекте класса VideoItem
+# должен быть локальный атрибут rating -
+# объект класса VideoRating.
+#
+# Пример использования классов
+# (эти строчки в программе не писать):
+#
+# v = VideoItem('Курс по Python ООП', 'Подробный курс по Python ООР', 'D:/videos/python_oop.mp4')
+# print(v.rating.rating) # 0
+# v.rating.rating = 5
+# print(v.rating.rating) # 5
+# title = v.title
+# descr = v.descr
+# v.rating.rating = 6  # ValueError
+#
+# P.S. В программе нужно объявить
+# только класс. На экран выводить
+# ничего не нужно.
+
+# class VideoItem:
+#     def __init__(self, title, descr, path):
+#         self.title = title
+#         self.descr = descr
+#         self.path = path
+#         self.rating = VideoRating()
+#
+# class VideoRating:
+#     def __init__(self):
+#         self.__rating = 0
+#
+#     @property
+#     def rating(self):
+#         return self.__rating
+#
+#     @rating.setter
+#     def rating(self, value):
+#         if not isinstance(value, int) or value < 0 or value > 5:
+#             raise ValueError('неверное присваиваемое значение')
+#         self.__rating = value
+
+# class First: pass
+# class Second(First): pass
+# class Third(Second): pass
+#
+# one = First()
+# two = Second()
+# three = Third()
+
+#
+# функция issubclass(obj, cls) возвращает True, если объект obj является объектом класса cls или какого-либо его базовых классов
+# print(issubclass(three, First)) # TypeError: issubclass() arg 1 must be a class Утверждение неверное.
+#
+# функция issubclass(cls1, cls2) возвращает True, если класс cls1 является подклассом класса cls2 но не его базовых классов
+# print(not issubclass(Third, First)) # False Утверждение неверное.
+#
+# функция issubclass(cls1, cls2) возвращает True, если класс cls1 является подклассом класса cls2
+# print(issubclass(Third, Second)) # True Утверждение верное.
+
+# функция isinstance(obj, cls) возвращает True, если объект obj является объектом класса cls но не его базовых классов
+# print(not isinstance(three, First)) # False Утверждение неверное.
+#
+# функция isinstance(cls1, cls2) возвращает True, если класс cls1 является подклассом класса cls2 или какого-либо его базовых классов
+# print(isinstance(Third, Third)) # False Утверждение неверное.
+#
+# функция isinstance(obj, cls) возвращает True, если объект obj является объектом класса cls
+# print(isinstance(three, Third)) # True Утверждение верное.
+
+# 9
+# Объявите в программе базовый класс с именем
+# IteratorAttrs для перебора всех локальных атрибутов
+# объектов класса. Напомню, что для этого используются
+# два магических метода:
+#
+# __iter__() - для получения объекта-итератора
+#   (в данном случае - это сам объект self)
+# __next__() - для перебора локальных атрибутов
+#   объекта self (используйте для этого словарь __dict__)
+#
+# Метод __next__() на каждой итерации должен
+# возвращать кортеж в формате: (имя атрибута, значение).
+#
+# Подсказка: здесь можно определить один метод
+# __iter__() как функцию-генератор.
+#
+# Объявите дочерний класс SmartPhone, объекты
+# которого создаются командой:
+#
+# phone = SmartPhone(model, size, memory)
+#
+# где
+# model - модель смартфона (строка);
+# size - габариты (ширина, длина) в виде
+#   кортежа двух чисел;
+# memory - размер ОЗУ (памяти), как целое число.
+# В каждом объекте класса SmartPhone должны
+# создаваться соответствующие локальные атрибуты:
+# model, size, memory.
+#
+# Благодаря наследованию от базового класса
+# IteratorAttrs, с объектами класса SmartPhone
+# должен выполняться оператор for:
+#
+# for attr, value in phone:
+#     print(attr, value)
+#
+# P.S. В программе нужно объявить
+# только классы. На экран выводить
+# ничего не нужно.
+
+# class IteratorAttrs:
+#     def __iter__(self):
+#         for attr in self.__dict__:
+#             yield (attr, getattr(self, attr))
+#
+# class SmartPhone(IteratorAttrs):
+#     def __init__(self, model, size, memory):
+#         self.model = model
+#         self.size = size
+#         self.memory = memory
+#
+# # Пример использования класса SmartPhone
+# phone = SmartPhone("SomeModel", (5, 7), 64)
+# for attr, value in phone:
+#     print(attr, value)
---
+++ OOPushka/SB_022_super.py
@@ -0,0 +1,379 @@
+# 3
+# Объявите в программе базовый класс
+# с именем Book, объекты которого
+# создаются командой:
+#
+# book = Book(title, author, pages, year)
+#
+# где
+# title - заголовок книги (строка);
+# author - автор книги (строка);
+# pages - число страниц (целое число);
+# year - год издания (целое число). В каждом
+# объекте класса Book должны формироваться
+# соответствующие локальные атрибуты:
+# title, author, pages, year.
+#
+# Объявите дочерний класс DigitBook от
+# класса Book, объекты которого
+# создаются командой:
+#
+# db = DigitBook(title, author, pages, year, size, frm)
+#
+# где дополнительные параметры
+# size - размер книги в байтах (целое число);
+# frm - формат книги (строка:
+# 'pdf', 'doc', 'fb2', 'txt'). В каждом
+# объекте класса DigitBook должны формироваться
+# соответствующие локальные атрибуты:
+# title, author, pages, year, size, frm.
+#
+# Инициализация локальных атрибутов
+# title, author, pages, year должна
+# выполняться в базовом классе Book,
+# а параметры size, frm инициализируются
+# в дочернем классе DigitBook.
+#
+# P.S. В программе нужно объявить
+# только классы. На экран выводить
+# ничего не нужно.
+
+# class Book:
+#     def __init__(self, title, author, pages, year):
+#         self.title = title
+#         self.author = author
+#         self.pages = pages
+#         self.year = year
+#
+#
+# class DigitBook(Book):
+#     def __init__(self, title, author, pages, year, size, frm):
+#         super().__init__(title, author, pages, year)
+#         self.size = size
+#         self.frm = frm
+
+# 4
+# Создается программа по учету склада.
+# Каждый предмет на складе должен описываться
+# базовым классом Thing. Объекты этого
+# класса создаются командой:
+#
+# th1 = Thing(name, weight)
+#
+# где
+# name - наименование предмета (строка);
+# weight - вес предмета (вещественное число).
+#
+# Для описания каждого конкретного вида
+# предметов, создаются дочерние классы
+# (на основе базового Thing):
+#
+# ArtObject - для представления арт-объектов;
+# Computer - для системных блоков компьютеров;
+# Auto - для автомобилей.
+#
+# Объекты этих классов создаются командами:
+#
+# obj = ArtObject(name, weight, author, date)  # author - автор (строка); date - дата создания (строка)
+# obj = Computer(name, weight, memory, cpu)    # memory - размер памяти (целое число); cpu - тип процессора (строка)
+# obj = Auto(name, weight, dims)               # dims - габариты, кортеж (width, length, height) - вещественные или целые числа
+#
+# На основе класса Auto создаются дочерние
+# классы Mercedes и Toyota, объекты
+# которых определяются командами:
+#
+# auto = Mercedes(name, weight, dims, model, old) # model - модель (строка); old - время использования, в годах (целое число)
+# auto = Toyota(name, weight, dims, model, wheel) # model - модель (строка); wheel - тип руля: True - леворульный, False - праворульный
+#
+# Во всех объектах классов должны создаваться
+# соответствующие локальные атрибуты:
+# name, weight и т.д.
+#
+# Инициализация атрибутов должна выполняться
+# в соответствующих классах (не должно
+# быть дублирования кода).
+#
+# P.S. В программе нужно объявить только
+# классы. На экран выводить ничего не нужно.
+
+# class Thing:
+#     def __init__(self, name, weight):
+#         self.name = name
+#         self.weight = weight
+#
+#
+# class ArtObject(Thing):
+#     def __init__(self, name, weight, author, date):
+#         super().__init__(name, weight)
+#         self.author = author
+#         self.date = date
+#
+#
+# class Computer(Thing):
+#     def __init__(self, name, weight, memory, cpu):
+#         super().__init__(name, weight)
+#         self.memory = memory
+#         self.cpu = cpu
+#
+#
+# class Auto(Thing):
+#     def __init__(self, name, weight, dims):
+#         super().__init__(name, weight)
+#         self.dims = dims
+#
+#
+# class Mercedes(Auto):
+#     def __init__(self, name, weight, dims, model, old):
+#         super().__init__(name, weight, dims)
+#         self.model = model
+#         self.old = old
+#
+#
+# class Toyota(Auto):
+#     def __init__(self, name, weight, dims, model, wheel):
+#         super().__init__(name, weight, dims)
+#         self.model = model
+#         self.wheel = wheel
+
+# 5
+# Вам поручено организовать представление
+# объектов для продажи в риэлтерских агентствах.
+# Для этого в программе нужно объявить базовый
+# класс SellItem, объекты которого
+# создаются командой:
+#
+# item = SellItem(name, price)
+#
+# где
+# name - название объекта продажи (строка);
+# price - цена продажи (число: целое или вещественное).
+#
+# Каждые конкретные типы объектов описываются
+# следующими классами, унаследованные от базового SellItem:
+#
+# House - дома;
+# Flat - квартиры;
+# Land - земельные участки.
+# Объекты этих классов создаются командами:
+#
+# house = House(name, price, material, square)
+# flat = Flat(name, price, size, rooms)
+# land = Land(name, price, square)
+#
+# В каждом объекте этих классов должны
+# формироваться соответствующие локальные
+# атрибуты: name, price и т.д.
+#
+# Формирование атрибутов name и price должно
+# выполняться в инициализаторе базового класса.
+#
+# Далее, объявить еще один класс с именем Agency,
+# объекты которого создаются командой:
+#
+# ag = Agency(name)
+#
+# где name - название агентства (строка).
+# В классе Agency объявить следующие методы:
+#
+# add_object(obj) - добавление нового
+# объекта недвижимости для продажи (один
+# из объектов классов: House, Flat, Land);
+# remove_object(obj) - удаление объекта
+# obj из списка объектов для продажи;
+# get_objects() - возвращает список из
+# всех объектов для продажи.
+#
+# Пример использования классов (эти
+# строчки в программе не писать):
+#
+# ag = Agency("Рога и копыта")
+# ag.add_object(Flat("квартира, 3к", 10000000, 121.5, 3))
+# ag.add_object(Flat("квартира, 2к", 8000000, 74.5, 2))
+# ag.add_object(Flat("квартира, 1к", 4000000, 54, 1))
+# ag.add_object(House("дом, кирпичный", price=35000000, material="кирпич", square=186.5))
+# ag.add_object(Land("участок под застройку", 3000000, 6.74))
+# for obj in ag.get_objects():
+#     print(obj.name)
+#
+# lst_houses = [x for x in ag.get_objects() if isinstance(x, House)] # выделение списка домов
+#
+# P.S. В программе нужно объявить
+# только классы. На экран выводить
+# ничего не нужно.
+
+# class SellItem:
+#     def __init__(self, name, price):
+#         self.name = name
+#         self.price = price
+#
+#
+# class House(SellItem):
+#     def __init__(self, name, price, material, square):
+#         super().__init__(name, price)
+#         self.material = material
+#         self.square = square
+#
+#
+# class Flat(SellItem):
+#     def __init__(self, name, price, size, rooms):
+#         super().__init__(name, price)
+#         self.size = size
+#         self.rooms = rooms
+#
+#
+# class Land(SellItem):
+#     def __init__(self, name, price, square):
+#         super().__init__(name, price)
+#         self.square = square
+#
+#
+# class Agency:
+#     def __init__(self, name):
+#         self.name = name
+#         self.objects = []
+#
+#     def add_object(self, obj):
+#         self.objects.append(obj)
+#
+#     def remove_object(self, obj):
+#         self.objects.remove(obj)
+#
+#     def get_objects(self):
+#         return self.objects
+
+# 6 аша команда создает небольшой фреймворк
+# для веб-сервера. Для этого был объявлен класс:
+#
+# class Router:
+#     app = {}
+#
+#     @classmethod
+#     def get(cls, path):
+#         return cls.app.get(path)
+#
+#     @classmethod
+#     def add_callback(cls, path, func):
+#         cls.app[path] = func
+#
+# И его предполагается использовать
+# следующим образом:
+#
+# @Callback('/', Router)
+# def index():
+#     return '<h1>Главная</h1>'
+#
+#
+# route = Router.get('/')
+# if route:
+#     ret = route()
+#     print(ret)
+#
+# Здесь Callback - это класс-декоратор
+# с параметрами:
+# path = '/' - маршрут;
+# router_cls = Router - класс роутера.
+#
+# Декоратор Callback должен обеспечивать
+# добавление функции (в примере index) в
+# словарь app класса Router. Ключом словаря
+# выступает маршрут (path), а значением -
+# ссылка на декорируемую функцию. Для этого
+# следует использовать метод add_callback
+# класса Router.
+#
+# Затем, из роутера (Router) методом get
+# выбирается ранее добавленная функция
+# (в примере index), и если она существует,
+# то вызывается с выводом результата в консоль.
+#
+# Ваша задача реализовать класс-декоратор Callback.
+#
+# Небольшая справка.
+#
+# Для реализации декоратора с параметрами
+# на уровне класса в инициализаторе
+# __init__(self, methods) прописываем
+# параметр для декоратора, а магический
+# метод __call__() объявляем для
+# декорирования функции:
+#
+# class Handler:
+#     def __init__(self, path, route_cls):
+#         # здесь нужные строчки
+#
+#     def __call__(self, func):
+#         # здесь строчки
+#
+# P.S. В программе нужно объявить
+# только классы. На экран выводить
+# ничего не нужно.
+
+# class Router:
+#     app = {}
+#
+#     @classmethod
+#     def get(cls, path):
+#         return cls.app.get(path)
+#
+#     @classmethod
+#     def add_callback(cls, path, func):
+#         cls.app[path] = func
+#
+# # здесь объявляйте декоратор Callback
+# class Callback:
+#     def __init__(self, path, router_cls):
+#         self.path = path
+#         self.router_cls = router_cls
+#
+#     def __call__(self, func):
+#         self.router_cls.add_callback(self.path, func)
+#         return func
+
+# 7
+# В программе объявлена функция integer_params
+# для класса Vector, которая применяет к каждому
+# методу класса декоратор integer_params_decorated:
+#
+# def integer_params(cls):
+#     methods = {k: v for k, v in cls.__dict__.items() if callable(v)}
+#     for k, v in methods.items():
+#         setattr(cls, k, integer_params_decorated(v))
+#
+#     return cls
+#
+#
+# @integer_params
+# class Vector:
+#     def __init__(self, *args):
+#         self.__coords = list(args)
+#
+#     def __getitem__(self, item):
+#         return self.__coords[item]
+#
+#     def __setitem__(self, key, value):
+#         self.__coords[key] = value
+#
+#     def set_coords(self, *coords, reverse=False):
+#         c = list(coords)
+#         self.__coords = c if not reverse else c[::-1]
+#
+# Декоратор integer_params_decorated должен проверять,
+# чтобы все передаваемые аргументы в методы класса
+# (кроме первого self) были целыми числами
+# (имели тип int). Если это не так, то должно
+# генерироваться исключение командой:
+#
+# raise TypeError("аргументы должны быть целыми числами")
+#
+# Ваша задача объявить эту функцию-декоратор.
+#
+# Пример использования класса
+# (эти строчки в программе не писать):
+#
+# vector = Vector(1, 2)
+# print(vector[1])
+# vector[1] = 20.4 # TypeError
+#
+# P.S. В программе нужно объявить
+# только класс. На экран выводить
+# ничего не нужно.
