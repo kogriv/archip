b1504a41fd22dff112b5a13f1db2120c34abbfd8
Grigory Kozhanov <kogriv@gmail.com>
2023-10-25 20:48:00 +0700

    oop course

--- OOPushka/SB_027_slots.py
+++ OOPushka/SB_027_slots.py
@@ -747,3 +747,169 @@
 # Vertex, Link, LinkedGraph, Station, LinkMetro.
 # На экран ничего выводить не нужно.
 
+class Vertex:
+    def __init__(self):
+        self._links = []
+
+    @property
+    def links(self):
+        return self._links
+
+class Link:
+    def __init__(self, v1, v2):
+        self._v1 = v1
+        self._v2 = v2
+        self._dist = 1
+
+    @property
+    def v1(self):
+        return self._v1
+
+    @property
+    def v2(self):
+        return self._v2
+
+    @property
+    def dist(self):
+        return self._dist
+
+
+class LinkedGraph:
+    def __init__(self):
+        self._links = []
+        self._vertex = []
+
+    def add_vertex(self, v):
+        if v not in self._vertex:
+            self._vertex.append(v)
+
+    def add_link(self, link):
+        # составим кортеж из связей, которые уже есть
+        t = tuple(filter(lambda x:
+                         (id(x.v1)==id(link.v1) and id(x.v2)==id(link.v2)) or \
+                         (id(x.v2)==id(link.v1) and id(x.v1)==id(link.v2)),
+                         self._links
+                         ))
+        if len(t) == 0:
+            self._links.append(link)
+            self.add_vertex(link.v1)
+            self.add_vertex(link.v2)
+            # добавим связи к вершинам
+            link.v1.links.append(link)
+            link.v2.links.append(link)
+
+
+    def find_path(self, start_v, stop_v, path=None):
+        # воспользуемся рекурсивной ф-ей
+        self._start_v = start_v
+        self._stop_v = stop_v
+        return self._next(self._start_v, None, [], [])
+
+    def _dist_path(self, links):
+        return sum([x.dist for x in links if x  is not None])
+
+
+    def _next(self, current, link_prev, current_path, current_links):
+        '''
+        Рекурсивная функция поиска кратчайшего пути
+
+        :param current:         текущая вершина - объект класса Vertex
+        :param link_prev:       предыдущая связь - объект класса Link
+        :param current_path:    список вершин Vertex
+        :param current_links:   список связей Link
+        :return: best_path, best_links - списки вершин и связей
+        '''
+
+        current_path.append(current)
+        if link_prev:
+            current_links.append(link_prev)
+
+        if current == self._stop_v:
+            return current_path, current_links
+
+        len_path = -1
+        best_path = []
+        best_links = []
+
+        # Проход по всем связям в графе
+        for link in self._links:
+            # рекурсивно проходим по всей связанной цепочке, до _stop_v
+            # остановка поиска на _stop_v гарантирована проверкой выше:
+            # if current == self._stop_v: return current_path, current_links
+            # рекурсия:
+            # если одна вершина связи является
+            # последней (current) на пути,
+            # а другая - еще не внесена в путь,
+            # то последней точкой станет не внесенная
+            # и "проваливаемся" в саму себя- в _next (по рекурсии)
+            if link.v1 == current and link.v2 not in current_path:
+                path, links = self._next(link.v2, link, current_path[:], current_links[:])
+            elif link.v2 == current and link.v1 not in current_path:
+                path, links = self._next(link.v1, link, current_path[:], current_links[:])
+            # иначе дошли до разрыва цеочки
+            # т.е. обе вершины связи - не связаны с
+            # построеным путем path
+            else:
+                # выходим из рекурсии
+                continue
+
+            # если конечная точка попала в путь
+            # и длина полученного по рекурсии
+            # пути меньше, чем ранее полученная длина
+            # Обновляем лучший путь и длину
+            if self._stop_v in path and (len_path > self._dist_path(links) or len_path == -1):
+                len_path = self._dist_path(links)
+                best_path = path[:]
+                best_links = links[:]
+            # print('path:',path)
+            # print('len_path:',len_path)
+
+            # если конечная точка не попала в построенный путь,
+            # это значит, что вся цепочка которая началась
+            # с первого link в цикле for, и была построена по рекурсии
+            # на этой итерации цикла - отбрасывается
+            # и мы переходим к другой связи link
+            # (к другой итерации цикла for)
+            # т.е. на каждой итерации цикла мы строим всю
+            # возможную цепочку связей
+
+        return best_path, best_links
+
+class Station(Vertex):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    def __repr__(self):
+        return self.name
+
+class LinkMetro(Link):
+    def __init__(self, v1, v2, dist):
+        super().__init__(v1, v2)
+        self._dist = dist
+
+map_metro = LinkedGraph()
+v1 = Station("Сретенский бульвар")
+v2 = Station("Тургеневская")
+v3 = Station("Чистые пруды")
+v4 = Station("Лубянка")
+v5 = Station("Кузнецкий мост")
+v6 = Station("Китай-город 1")
+v7 = Station("Китай-город 2")
+
+map_metro.add_link(LinkMetro(v1, v2, 1))
+map_metro.add_link(LinkMetro(v2, v3, 1))
+map_metro.add_link(LinkMetro(v1, v3, 1))
+
+map_metro.add_link(LinkMetro(v4, v5, 1))
+map_metro.add_link(LinkMetro(v6, v7, 1))
+
+map_metro.add_link(LinkMetro(v2, v7, 5))
+map_metro.add_link(LinkMetro(v3, v4, 3))
+map_metro.add_link(LinkMetro(v5, v6, 3))
+
+print(len(map_metro._links))
+print(len(map_metro._vertex))
+path = map_metro.find_path(v1, v6)  # от сретенского бульвара до китай-город 1
+print(path[0])    # [Сретенский бульвар, Тургеневская, Китай-город 2, Китай-город 1]
+print(sum([x.dist for x in path[1]]))  # 7
