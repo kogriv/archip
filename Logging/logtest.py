de321f526fe4a0f23b8a88d27f1ba45714511741
Grigory Kozhanov <kogriv@gmail.com>
2023-12-30 08:27:43 +0700

    archip upd

--- Logging/logtest.py
+++ Logging/logtest.py
@@ -1,6 +1,17 @@
 import logging
-logging.basicConfig()
+import requests
+
+logging.basicConfig(level='DEBUG')
+
 logger = logging.getLogger()
-logger.setLevel('DEBUG')
-logger.debug('test logrecord DEBUG')
-logger.warning('test logrecord WARNING')
+
+def main(name):
+    logger.debug(f'Enter in the main() function: name = {name}')
+    r = requests.get('https://www.google.ru')
+
+    for key in logging.Logger.manager.loggerDict:
+        print(key)
+
+
+if __name__ == '__main__':
+    main('func_name_specified')
