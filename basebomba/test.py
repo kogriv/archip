db98c7dc9dc8223694291bc6adf7eed24b5ce004
Grigory Kozhanov <kogriv@gmail.com>
2024-09-28 20:06:19 +0700

    upd

---
+++ basebomba/test.py
@@ -0,0 +1,26 @@
+# b = [45, 37, 100]
+# b.sort(reverse=True)
+# print(b)
+# c = input().split()
+# print(c)
+# c = map(int,c)
+# print(c)
+# c = list(c).sort()
+# print(c)
+# print(' '.join(map(str,sorted(list(map(int,input().split()))))))
+
+# l = [3,4,5,6]
+# # l.reverse()
+# print(l.reverse())
+
+# marks = [2, 3, 4, 3, 5, 2]
+# marks[2:4] = 10, 20
+# print(marks)
+
+# v = [1205, 1101, 1434, 1320, 923, 874]
+# vv = v[:3]
+# print(vv)
+
+m = [0, 1, 2, 3, 4, 5, 6, 7]
+m.insert(1,'b')
+print(m)
---
+++ np/01_create.py
@@ -0,0 +1,11 @@
+
+# my_list = [1, 2, 3, 4, 5]
+# my_array = np.array(my_list)
+# print(my_array)
+# ar = np.arange(1,20,2)
+# ar = np.linspace(1,10,19)
+# ar = np.linspace(-5,5,10)
+
+import numpy as np
+ar = np.arange(10,0,-1)
+print(ar)
